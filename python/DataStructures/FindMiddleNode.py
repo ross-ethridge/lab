8788ddd33f5d2112a708953fa14563b37c3c79bc
Ross Ethridge <ross.ethridge@gmail.com>
2025-08-23 14:44:25 -0500

    Find Middle Node

---
+++ python/DataStructures/FindMiddleNode.py
@@ -0,0 +1,43 @@
+#! /usr/bin/env python3
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+        
+
+class LinkedList:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+
+        
+    def append(self, value):
+        new_node = Node(value)
+        if self.head == None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        return True
+    
+    def find_middle_node(self):
+        
+        slow = self.head
+        fast = self.head
+        
+        if slow is not None:
+            while fast is not None and fast.next is not None:
+                fast = fast.next.next
+                slow = slow.next
+            return slow
+
+
+my_linked_list = LinkedList(1)
+my_linked_list.append(2)
+my_linked_list.append(3)
+my_linked_list.append(4)
+my_linked_list.append(5)
+
+print( my_linked_list.find_middle_node().value )
