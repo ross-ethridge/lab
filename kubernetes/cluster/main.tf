aeccd4a0eb531dc656e413ffa60ebd4157544dd8
Ross Ethridge <ross.ethridge@gmail.com>
2025-08-30 17:59:31 +0000

    Adding install of etcdctl to cloudInit

--- kubernetes/cluster/main.tf
+++ kubernetes/cluster/main.tf
@@ -16,13 +16,14 @@ provider "lxd" {
     address = "https://192.168.2.2:8443/"
     default = true
 
-    // export TF_VAR_lxd_token='token value...'
+    // server: lxc config trust add --name "terraform-token"
+    // client: export TF_VAR_lxd_token="tokenValue..."
     token = var.lxd_token
   }
 }
 
 
-// Storage pool for instance
+// Storage pool for kubemaster instance
 resource "lxd_storage_pool" "kubemaster_pool" {
   name   = "kubemaster-pool"
   driver = "zfs"
@@ -53,21 +54,48 @@ resource "lxd_instance" "kubemaster" {
     // Or embed directly:
     "user.user-data" = <<-EOF
       #cloud-config
+      package_update: true
       packages:
-        - build-essential
-        - openssh-server
-
+        - apt-transport-https
+        - ca-certificates
+        - curl
+        - wget
+        - gnupg
+        - lsb-release
+      
       users:
         - name: rossethridge
           groups: sudo,adm
           sudo: ["ALL=(ALL) NOPASSWD:ALL"]
           shell: /bin/bash
           ssh_authorized_keys:
-            - 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDN1wrYOvWiCDf7hLfN/rIq1sPyS25bLKa0dCHGEvcO/banPy7ooHCqVJgHeQNUh8gkBKSGzYoVObbrt6AIaVYndRPQhGJ5BvR1sKTxt/NOnKm2Ya0HwEK1dYX3weNzCQxq0MA8dX0HybjABRikd1+4845FjVqsWEL6hGZSS3vPFl9J/f4CO0qMk5DF3O4wQozppSaoWJtWpGsl0stVQqeTGw9f9QJ0MxQvlvNPuoFazRbvdPNjfCjnd8AEBZRJBBUEmwRhHPgBAyr0c9Bxh88yYaFXgST81/mHSjC8SAHJXQf06AQv3dxLzCPduQJa297qE0p4rE7cABGalgo69pE/ rethridge@rethridge-pc'
-            - 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCshYoGv5o0T0Z8vFQvwu1vFSlWZOZEuH40FVUHH0gg1jiQhQ2JP1Sjydnj2K9ejzrzk7eJyTjTYceI5BYBj6ey02VoY1PKBYjgbYb1U/JVpJ5fP9OYzn1l3plySLW7pTB7UTE5+pBToB5tVr/D2GDnGo138+eAG89gaFZbwyczTlxcg7J+cnd1zZymSOOUUSqOoSNWtTf4lcfmOKf5sM9OGBMQPS2CWUfI39jNEOsC+BmcqLcskdwuEGAEdMxSvIvo9Otrww9wBaG3w2cvTnlrAyAGURI0w+Nt1+AOgMwoSMZ5RqjEcnxRWyGwuHI7AubqLTjq2ZSjejby8bqu8F7t rossethridge@microcloud-01'
-      
+            - "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDN1wrYOvWiCDf7hLfN/rIq1sPyS25bLKa0dCHGEvcO/banPy7ooHCqVJgHeQNUh8gkBKSGzYoVObbrt6AIaVYndRPQhGJ5BvR1sKTxt/NOnKm2Ya0HwEK1dYX3weNzCQxq0MA8dX0HybjABRikd1+4845FjVqsWEL6hGZSS3vPFl9J/f4CO0qMk5DF3O4wQozppSaoWJtWpGsl0stVQqeTGw9f9QJ0MxQvlvNPuoFazRbvdPNjfCjnd8AEBZRJBBUEmwRhHPgBAyr0c9Bxh88yYaFXgST81/mHSjC8SAHJXQf06AQv3dxLzCPduQJa297qE0p4rE7cABGalgo69pE/ rethridge@rethridge-pc"
+            - "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCshYoGv5o0T0Z8vFQvwu1vFSlWZOZEuH40FVUHH0gg1jiQhQ2JP1Sjydnj2K9ejzrzk7eJyTjTYceI5BYBj6ey02VoY1PKBYjgbYb1U/JVpJ5fP9OYzn1l3plySLW7pTB7UTE5+pBToB5tVr/D2GDnGo138+eAG89gaFZbwyczTlxcg7J+cnd1zZymSOOUUSqOoSNWtTf4lcfmOKf5sM9OGBMQPS2CWUfI39jNEOsC+BmcqLcskdwuEGAEdMxSvIvo9Otrww9wBaG3w2cvTnlrAyAGURI0w+Nt1+AOgMwoSMZ5RqjEcnxRWyGwuHI7AubqLTjq2ZSjejby8bqu8F7t rossethridge@microcloud-01"
+
       runcmd:
-        - systemctl emable sshd --now
+        - mkdir -p /etc/apt/keyrings
+        - curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+        - curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.34/deb/Release.key | gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+        - bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)" --prefix=/usr/local
+        - cp /usr/local/share/oh-my-bash/bashrc ~rossethridge/.bashrc
+        - echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list
+        - echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.34/deb/ /' | tee /etc/apt/sources.list.d/kubernetes.list
+        - wget https://github.com/etcd-io/etcd/releases/download/v3.6.4/etcd-v3.6.4-linux-amd64.tar.gz
+        - tar -xvzf etcd-v3.6.4-linux-amd64.tar.gz
+        - cp etcd-v3.6.4-linux-amd64/{etcd,etcdctl} /usr/local/bin/
+        - apt update
+        - apt install -y kubelet kubeadm kubectl containerd.io
+        - containerd config default | sudo tee /etc/containerd/config.toml > /dev/null
+        - sed -i 's/disabled_plugins = \["cri"\]/#disabled_plugins = \["cri"\]/' /etc/containerd/config.toml
+        - sed -i 's/SystemdCgroup = false/SystemdCgroup = true/g' /etc/containerd/config.toml
+        - apt-mark hold kubelet kubeadm kubectl containerd.io
+        - sysctl -w net.ipv4.ip_forward=1
+        - echo "net.ipv4.ip_forward = 1" | tee -a /etc/sysctl.conf
+        - sysctl -p
+        - systemctl enable containerd
+        - systemctl enable kubelet
+        - systemctl restart containerd.service
+        - systemctl restart kubelet.service
 
       EOF
   }
@@ -80,7 +108,7 @@ resource "lxd_instance" "kubemaster" {
 }
 
 
-// Storage pool for instance
+// Storage pool for kubeworker instance
 resource "lxd_storage_pool" "kubeworker_pool" {
   count  = 3
   name   = "kubeworker${count.index}-pool"
@@ -113,21 +141,48 @@ resource "lxd_instance" "kubeworker" {
     // Or embed directly:
     "user.user-data" = <<-EOF
       #cloud-config
+      package_update: true
       packages:
-        - build-essential
-        - openssh-server
-
+        - apt-transport-https
+        - ca-certificates
+        - curl
+        - wget
+        - gnupg
+        - lsb-release
+      
       users:
         - name: rossethridge
           groups: sudo,adm
           sudo: ["ALL=(ALL) NOPASSWD:ALL"]
           shell: /bin/bash
           ssh_authorized_keys:
-            - 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDN1wrYOvWiCDf7hLfN/rIq1sPyS25bLKa0dCHGEvcO/banPy7ooHCqVJgHeQNUh8gkBKSGzYoVObbrt6AIaVYndRPQhGJ5BvR1sKTxt/NOnKm2Ya0HwEK1dYX3weNzCQxq0MA8dX0HybjABRikd1+4845FjVqsWEL6hGZSS3vPFl9J/f4CO0qMk5DF3O4wQozppSaoWJtWpGsl0stVQqeTGw9f9QJ0MxQvlvNPuoFazRbvdPNjfCjnd8AEBZRJBBUEmwRhHPgBAyr0c9Bxh88yYaFXgST81/mHSjC8SAHJXQf06AQv3dxLzCPduQJa297qE0p4rE7cABGalgo69pE/ rethridge@rethridge-pc'
-            - 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCshYoGv5o0T0Z8vFQvwu1vFSlWZOZEuH40FVUHH0gg1jiQhQ2JP1Sjydnj2K9ejzrzk7eJyTjTYceI5BYBj6ey02VoY1PKBYjgbYb1U/JVpJ5fP9OYzn1l3plySLW7pTB7UTE5+pBToB5tVr/D2GDnGo138+eAG89gaFZbwyczTlxcg7J+cnd1zZymSOOUUSqOoSNWtTf4lcfmOKf5sM9OGBMQPS2CWUfI39jNEOsC+BmcqLcskdwuEGAEdMxSvIvo9Otrww9wBaG3w2cvTnlrAyAGURI0w+Nt1+AOgMwoSMZ5RqjEcnxRWyGwuHI7AubqLTjq2ZSjejby8bqu8F7t rossethridge@microcloud-01'
+            - "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDN1wrYOvWiCDf7hLfN/rIq1sPyS25bLKa0dCHGEvcO/banPy7ooHCqVJgHeQNUh8gkBKSGzYoVObbrt6AIaVYndRPQhGJ5BvR1sKTxt/NOnKm2Ya0HwEK1dYX3weNzCQxq0MA8dX0HybjABRikd1+4845FjVqsWEL6hGZSS3vPFl9J/f4CO0qMk5DF3O4wQozppSaoWJtWpGsl0stVQqeTGw9f9QJ0MxQvlvNPuoFazRbvdPNjfCjnd8AEBZRJBBUEmwRhHPgBAyr0c9Bxh88yYaFXgST81/mHSjC8SAHJXQf06AQv3dxLzCPduQJa297qE0p4rE7cABGalgo69pE/ rethridge@rethridge-pc"
+            - "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCshYoGv5o0T0Z8vFQvwu1vFSlWZOZEuH40FVUHH0gg1jiQhQ2JP1Sjydnj2K9ejzrzk7eJyTjTYceI5BYBj6ey02VoY1PKBYjgbYb1U/JVpJ5fP9OYzn1l3plySLW7pTB7UTE5+pBToB5tVr/D2GDnGo138+eAG89gaFZbwyczTlxcg7J+cnd1zZymSOOUUSqOoSNWtTf4lcfmOKf5sM9OGBMQPS2CWUfI39jNEOsC+BmcqLcskdwuEGAEdMxSvIvo9Otrww9wBaG3w2cvTnlrAyAGURI0w+Nt1+AOgMwoSMZ5RqjEcnxRWyGwuHI7AubqLTjq2ZSjejby8bqu8F7t rossethridge@microcloud-01"
 
       runcmd:
-        - systemctl emable sshd --now
+        - mkdir -p /etc/apt/keyrings
+        - curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+        - curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.34/deb/Release.key | gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+        - bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)" --prefix=/usr/local
+        - cp /usr/local/share/oh-my-bash/bashrc ~rossethridge/.bashrc
+        - echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list
+        - echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.34/deb/ /' | tee /etc/apt/sources.list.d/kubernetes.list
+        - wget https://github.com/etcd-io/etcd/releases/download/v3.6.4/etcd-v3.6.4-linux-amd64.tar.gz
+        - tar -xvzf etcd-v3.6.4-linux-amd64.tar.gz
+        - cp etcd-v3.6.4-linux-amd64/{etcd,etcdctl} /usr/local/bin/
+        - apt update
+        - apt install -y kubelet kubeadm kubectl containerd.io
+        - containerd config default | sudo tee /etc/containerd/config.toml > /dev/null
+        - sed -i 's/disabled_plugins = \["cri"\]/#disabled_plugins = \["cri"\]/' /etc/containerd/config.toml
+        - sed -i 's/SystemdCgroup = false/SystemdCgroup = true/g' /etc/containerd/config.toml
+        - apt-mark hold kubelet kubeadm kubectl containerd.io
+        - sysctl -w net.ipv4.ip_forward=1
+        - echo "net.ipv4.ip_forward = 1" | tee -a /etc/sysctl.conf
+        - sysctl -p
+        - systemctl enable containerd
+        - systemctl enable kubelet
+        - systemctl restart containerd.service
+        - systemctl restart kubelet.service
 
       EOF
   }
