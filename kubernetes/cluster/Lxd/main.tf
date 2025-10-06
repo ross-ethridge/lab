5c0445ee1877eb3e7ed3903927c6c59bddd2f6fe
Ross Ethridge <ross.ethridge@gmail.com>
2025-10-06 16:44:57 -0500

    Moving backend to AWS

--- kubernetes/cluster/Lxd/main.tf
+++ kubernetes/cluster/Lxd/main.tf
@@ -1,9 +1,20 @@
 terraform {
-  backend "local" {}
+  #backend "local" {}
+  backend "s3" {
+    bucket  = "ross-lab-tfstate"
+    key     = "kube/cluster.terraform.tfstate"
+    region  = "us-east-2"
+    encrypt = true
+  }
+
   required_providers {
     lxd = {
       source  = "terraform-lxd/lxd"
-      version = "2.5.0"
+      version = "~> 2"
+    }
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4"
     }
   }
 }
